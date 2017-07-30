2aa2bdf4a373e58adc60e39f15c7016f0e15ee59
alibaba124 <30572492+alibaba124@users.noreply.github.com>
2017-07-30 08:00:51 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
+const TelegramBot = require('node-telegram-bot-api');
 
+ const token = '302900357:AAHt9OvChQgyYDnb1meLWqY5K3_79aonVpI'; 
+
+const bot = new TelegramBot(token, { polling: true });
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+var Storage = require('node-storage');
+var store = new Storage('store');
+
+
+bot.onText(/\$(\w+)\n\$([\w\W]*)/gu, (msg, regres)=>{
+	const chatId = msg.chat.id;
+    store.put(regres['1'], regres['2']);
+	
+
+});
+
+bot.onText(/\/(\w+)/, (msg, regresult) => {
+	const chatId = msg.chat.id;
+	var temp = store.get(regresult['1']);
+	if (temp != null) {
+		bot.sendMessage(chatId, temp);
+	}
+
+});
